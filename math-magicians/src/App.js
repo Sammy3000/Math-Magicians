c03eecaed9c0062447195472ba8339417e8e81ac
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-13 14:09:57 +0300

    modify app js file

--- math-magicians/src/App.js
+++ math-magicians/src/App.js
@@ -1,6 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello REACT</p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
