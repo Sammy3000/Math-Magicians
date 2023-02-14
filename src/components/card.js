d12bf3fa1ed362bac0c0d738af7c50c85ee4e6c0
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-14 19:44:53 +0300

    fix linter errors.remove all the boilerplate that got created by the CRA

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,7 @@
-/* eslint-disable */
-import "./card.css";
+import './card.css';
+
 const containerCover = (prop) => {
-  const cover = "container-cover " + prop.className;
+  const cover = `container-cover ${prop.className}`;
   return <div className={cover}>{prop.children}</div>;
 };
 export default containerCover;
