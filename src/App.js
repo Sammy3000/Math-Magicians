97f956b6dd161e30bc9cafd76c59df1f5951a4fa
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-16 22:30:51 +0300

    refactor with hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import Calc from './components/calculator';
+/*eslint-disable*/
+import React, { Component } from "react";
+import "./App.css";
+import Calc from "./components/calculator";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <Calc />;
-  }
-}
+const App = () => {
+  return <Calc />;
+};
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,119 +1,92 @@
-import React, { Component } from 'react';
-import './calculator.css';
-import calculate from '../logic/calculate';
-import Card from './card';
+/*eslint-disable*/
+import React, { useState } from "react";
+import "./calculator.css";
+import calculate from "../logic/calculate";
+import Card from "./card";
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const Calculator = () => {
+  const [currentState, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const { total, next, operation } = currentState;
 
-    this.buttonClicked = (e) => {
-      const clickedButton = e.target.innerText;
-      const outcome = calculate(this.state, clickedButton);
-      this.setState(outcome);
-    };
-  }
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <Card>
-        <div className="container-grid">
-          <div className="output">
-            <div>{total}</div>
-            <div className="previousOperand">{operation}</div>
-            <div className="currentOperand">{next}</div>
-          </div>
-          <button type="button" onClick={this.buttonClicked}>
-            AC
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            +/-
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            %
-          </button>
-          <button
-            type="button"
-            className="operand"
-            onClick={this.buttonClicked}
-          >
-            ÷
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            7
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            8
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            9
-          </button>
-          <button
-            type="button"
-            className="operand"
-            onClick={this.buttonClicked}
-          >
-            x
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            4
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            5
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            6
-          </button>
-          <button
-            type="button"
-            className="operand"
-            onClick={this.buttonClicked}
-          >
-            -
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            1
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            2
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            3
-          </button>
-          <button
-            type="button"
-            className="operand"
-            onClick={this.buttonClicked}
-          >
-            +
-          </button>
-          <button
-            type="submit"
-            className="span-two"
-            onClick={this.buttonClicked}
-          >
-            0
-          </button>
-          <button type="button" onClick={this.buttonClicked}>
-            .
-          </button>
-          <button
-            type="button"
-            className="operand"
-            onClick={this.buttonClicked}
-          >
-            =
-          </button>
+  const buttonClicked = (e) => {
+    const clickedButton = e.target.innerText;
+    const result = calculate(currentState, clickedButton);
+    setState(result);
+  };
+  return (
+    <Card>
+      <div className="container-grid">
+        <div className="output">
+          {total}
+          {""}
+          {operation}
+          {""}
+          {next}
         </div>
-      </Card>
-    );
-  }
-}
+        <button type="button" onClick={buttonClicked}>
+          AC
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          +/-
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          %
+        </button>
+        <button type="button" className="operand" onClick={buttonClicked}>
+          ÷
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          7
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          8
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          9
+        </button>
+        <button type="button" className="operand" onClick={buttonClicked}>
+          x
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          4
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          5
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          6
+        </button>
+        <button type="button" className="operand" onClick={buttonClicked}>
+          -
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          1
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          2
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          3
+        </button>
+        <button type="button" className="operand" onClick={buttonClicked}>
+          +
+        </button>
+        <button type="submit" className="span-two" onClick={buttonClicked}>
+          0
+        </button>
+        <button type="button" onClick={buttonClicked}>
+          .
+        </button>
+        <button type="button" className="operand" onClick={buttonClicked}>
+          =
+        </button>
+      </div>
+    </Card>
+  );
+};
 
 export default Calculator;
